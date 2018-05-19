19db7277a4bf7cdc47db604a18a7de3bf0fa5014
Peggy Lee <peggyleesix@gmail.com>
2018-05-19 19:08:12 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -13,3 +13,17 @@ end
 #    break
 #  end
 #end
+
+#while
+#counter = 0
+#while counter < 20
+#  puts "The current number is less than 20."
+#  counter += 1
+#end
+
+#until
+#counter = 0
+#until counter == 20
+#  puts "The current number is less than 20."
+#  counter += 1
+#end
