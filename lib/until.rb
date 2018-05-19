dc121efdc8dd3140446ac1b3dc35027f695839b0
Peggy Lee <peggyleesix@gmail.com>
2018-05-19 19:04:35 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,15 @@
 def using_until
         levitation_force = 6
         #your code here
-    
+
 end
 
+#counter = 0
+
+#loop do
+#  counter += 1
+#  puts "Iteration #{counter} of the loop"
+#  if counter >= 10
+#    break
+#  end
+#end
